a9fd23a5495a7d794fef7332824d5584253361f4
profAndreSouza <andre.souza@sp.senai.br>
2024-09-18 15:36:07 -0300

    updated

--- database/migrations/2024_08_28_191103_create_topic_tags_table.php
+++ database/migrations/2024_08_28_191103_create_topic_tags_table.php
@@ -14,10 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('topic_tags', function (Blueprint $table) {
+            $table->id();
             $table->unsignedBigInteger('tag_id');
             $table->unsignedBigInteger('topic_id');
             $table->foreign('tag_id')->references('id')->on('tags');
             $table->foreign('topic_id')->references('id')->on('topics');
+            // $table->foreignId('tag_id')->constrained();
+            // $table->foreignId('topic_id')->constrained();
+            $table->timestamps();
         });
     }
 
